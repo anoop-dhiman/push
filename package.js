caa2ccd026759a78997e2cedfa7ecf0b35f11deb
anoop-dhiman <dhiman.anoop09@gmail.com>
2018-07-20 19:33:07 +0530

    updating package name

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'raix:push',
+  name: 'anoop:push',
   version: '0.0.0-semantic-release',
   summary: 'Isomorphic Push notifications for APN and GCM',
   git: 'https://github.com/raix/push.git'
